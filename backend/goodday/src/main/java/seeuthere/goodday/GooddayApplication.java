a27cba38975b82d715dd20b5319559ecde00ad2f
cjy17 <cjy1705@naver.com>
2021-07-06 17:08:43 +0900

    feat: 백엔드 스프링 환경 설정

---
+++ backend/goodday/src/main/java/seeuthere/goodday/GooddayApplication.java
@@ -0,0 +1,13 @@
+package seeuthere.goodday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooddayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooddayApplication.class, args);
+	}
+
+}

b1d8bb41384dbaf94f6327c8c17bb033870a0f71
0imbean0 <tjsqls2003@gmail.com>
2021-07-07 16:19:43 +0900

    chore: 개발 환경 설정
    Co-authored-by: 365kim <365kim@users.noreply.github.com>

---
+++ frontend/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  entry: {
+    main: "./src/index",
+  },
+  output: {
+    path: path.join(__dirname, "dist"), // __dirname: 현재 실행 중인 폴더 경로
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 5000,
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["last 2 versions"],
+                },
+              },
+            ],
+            "@babel/preset-react",
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify("v0.1.0"),
+    }),
+    new CleanWebpackPlugin()(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+  },
+};

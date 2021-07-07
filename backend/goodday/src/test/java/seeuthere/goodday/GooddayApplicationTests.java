2e60a2a08527fef856ef71d167cb6ff4bfafeb41
cjy17 <cjy1705@naver.com>
2021-07-07 14:47:53 +0900

    feat: 백엔드 환경 설정

---
+++ backend/goodday/src/test/java/seeuthere/goodday/GooddayApplicationTests.java
@@ -0,0 +1,13 @@
+package seeuthere.goodday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooddayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

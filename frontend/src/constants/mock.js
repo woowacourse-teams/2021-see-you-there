ef846ddbdb3bf36c73775e92e6d75a164d3a6a63
0imbean0 <tjsqls2003@gmail.com>
2021-07-12 23:08:12 +0900

    feat: Mock 데이터 추가

---
+++ frontend/src/constants/mock.js
@@ -0,0 +1,52 @@
+import { Image } from '../assets';
+
+export const MOCK_PARTICIPANT_LIST = [
+  {
+    id: 0,
+    name: '임심바심바',
+    address: '서울시 송파구 잠실 루터회관 999-999',
+    x: '0.00',
+    y: '0.00',
+    avatar: Image.avatar,
+  },
+  {
+    id: 1,
+    name: '김하루하루',
+    address: '서울시 송파구 잠실 루터회관 888-888',
+    x: '0.00',
+    y: '0.00',
+    avatar: Image.avatar,
+  },
+  {
+    id: 2,
+    name: '안춘식춘식',
+    address: '서울시 송파구 잠실 루터회관 ',
+    x: '0.00',
+    y: '0.00',
+    avatar: Image.avatar,
+  },
+  {
+    id: 3,
+    name: '허와입와입',
+    address: '서울시 송파구 잠실 루터회관',
+    x: '0.00',
+    y: '0.00',
+    avatar: Image.avatar,
+  },
+  {
+    id: 4,
+    name: '최영이영이',
+    address: '서울시 송파구 잠실 루터회관',
+    x: '0.00',
+    y: '0.00',
+    avatar: Image.avatar,
+  },
+  {
+    id: 5,
+    name: '김멍토멍토',
+    address: '서울시 송파구 잠실 루터회관',
+    x: '0.00',
+    y: '0.00',
+    avatar: Image.avatar,
+  },
+];

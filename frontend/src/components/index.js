510c3de678a0170095c7965da3ef44049ae11cd8
0imbean0 <tjsqls2003@gmail.com>
2021-07-12 23:07:17 +0900

    feat: ParticipantList 컴포넌트 추가

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,5 @@
 export * as Icon from './icons';
 
-export { Input } from './atoms/Input';
 export { ButtonRound } from './atoms/ButtonRound';
 export { Notice } from './atoms/Notice';
 export { Input } from './atoms/Input';
@@ -8,3 +7,4 @@ export { Modal } from './atoms/Modal';
 
 export { NavBar } from './molecules/NavBar';
 export { Confirm } from './molecules/Confirm';
+export { ParticipantList } from './molecules/ParticipantList';

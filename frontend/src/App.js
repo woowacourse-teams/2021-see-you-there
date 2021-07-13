8cadfe0a33d8691099bc3b98bd6b62ab0e79d0f9
0imbean0 <tjsqls2003@gmail.com>
2021-07-13 18:28:16 +0900

    chore: TODO 작성
    Co-authored-by: 365kim <365kim@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import { NavBar } from './components';
 import { ROUTE } from './constants';
 
 export const App = () => {
+  // TODO: const {participants, add, delete} = useParticipants()
+
   return (
     <Router>
       <NavBar />
@@ -13,6 +15,17 @@ export const App = () => {
         <Route exact path={ROUTE.HOME.PATH}>
           <HomePage />
         </Route>
+        // TODO: 중간지점 찾기 페이지
+        {/* <Route exact path={ROUTE.MIDPOINT.PATH}>
+           <MidpointPage participants={participants} />
+         </Route> */}
+        // TODO: 로그인 / 로그아웃 페이지
+        {/* <Route exact path={ROUTE.LOGIN.PATH}>
+          <LoginPage/>
+        </Route>
+        <Route exact path={ROUTE.LOGOUT.PATH}>
+          <LogoutPage/>
+        </Route> */}
       </Switch>
     </Router>
   );

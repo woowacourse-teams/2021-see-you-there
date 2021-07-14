61026f8ce39abd72c4baf5d282c999fb4f417688
YeongBeom Heo <47732237+hybeom0720@users.noreply.github.com>
2021-07-14 16:40:56 +0900

    [3] 주소를 좌표로 전환해주는 기능 (#38)
    * refactor: LocationRequester 리팩터링
    
    * feat: 주소를 좌표로 바꿔주는 기능 추가
    
    * feat: 주소 변경 기능을 연결
    
    Co-authored-by: 멍토 <daum7766@naver.com>

---
+++ backend/goodday/src/main/java/seeuthere/goodday/location/dto/Address.java
@@ -0,0 +1,24 @@
+package seeuthere.goodday.location.dto;
+
+public class Address {
+
+    private String addressName;
+
+    public Address() {
+    }
+
+    public Address(String addressName) {
+        this.addressName = addressName;
+    }
+
+    public String getAddressName() {
+        return addressName;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+            "addressName='" + addressName + '\'' +
+            '}';
+    }
+}

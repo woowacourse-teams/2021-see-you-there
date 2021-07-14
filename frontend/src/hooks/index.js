cf40449f80c22954ec78b7968588ae9b0539d26e
365kim <365listener@gmail.com>
2021-07-14 22:56:01 +0900

    feat: usePariticipantRemoveConfirm 커스텀 훅 추가
    Co-authored-by: 0imbean0 <0imbean0@users.noreply.github.com>

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -2,4 +2,5 @@ export { useConfirm } from './useConfirm';
 export { useMapView } from './useMapView';
 export { useModal } from './useModal';
 export { useParticipantForm } from './useParticipantForm';
+export { useParticipantRemoveConfirm } from './useParticipantRemoveConfirm';
 export { useParticipants } from './useParticipants';

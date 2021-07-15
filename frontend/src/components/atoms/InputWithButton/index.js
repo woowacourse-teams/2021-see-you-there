f70129631c3e7c7f10c95db57f4b6961bde86715
365kim <365listener@gmail.com>
2021-07-15 17:23:22 +0900

    refactor: InputWithButton 컴포넌트 props 추가

--- frontend/src/components/atoms/InputWithButton/index.js
+++ frontend/src/components/atoms/InputWithButton/index.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 import { Container } from './style';
 
 export const InputWithButton = (props) => {
-  const { name, label, Icon, width, onClickButton, buttonIcon, ...attrs } = props;
+  const { name, label, Icon, width, buttonType, onClickButton, buttonIcon, ...attrs } = props;
 
   return (
     <Container width={width} hasIcon={!!Icon}>
       <input id={name} name={name} {...attrs} />
       {label && <label htmlFor={name}>{label}</label>}
       {Icon}
-      <button onClick={onClickButton}>{buttonIcon}</button>
+      <button type={buttonType} onClick={onClickButton}>
+        {buttonIcon}
+      </button>
     </Container>
   );
 };
@@ -22,8 +24,11 @@ InputWithButton.propTypes = {
   Icon: PropTypes.node,
   width: PropTypes.string,
   buttonIcon: PropTypes.node,
+  buttonType: PropTypes.oneOf(['submit', 'button', 'reset']),
 };
 
 InputWithButton.defaultProps = {
   width: '100%',
+  buttonType: 'submit',
+  onClickButton: () => {},
 };

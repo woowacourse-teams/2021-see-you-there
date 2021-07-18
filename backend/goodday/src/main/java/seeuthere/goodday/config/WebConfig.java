04bbf75a73c4e4f1452447dab542c9917dbb9805
멍토 <daum7766@naver.com>
2021-07-18 12:04:12 +0900

    fix: strict origin cors 문제해결 시도 1

--- backend/goodday/src/main/java/seeuthere/goodday/config/WebConfig.java
+++ backend/goodday/src/main/java/seeuthere/goodday/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:9000");
+            .allowedOrigins("http://localhost:9000", "192.168.1.30");
     }
 }

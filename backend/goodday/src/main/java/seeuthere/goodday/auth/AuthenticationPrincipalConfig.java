602866303c7aaefbabb178790df31935dfd4ecbe
YeongBeom Heo <47732237+hybeom0720@users.noreply.github.com>
2021-07-21 14:13:46 +0900

    fix: 서버에서 cors 문제 해결 (#62)

--- backend/goodday/src/main/java/seeuthere/goodday/auth/AuthenticationPrincipalConfig.java
+++ backend/goodday/src/main/java/seeuthere/goodday/auth/AuthenticationPrincipalConfig.java
@@ -18,6 +18,7 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MemberInterceptor(authService))
+            .excludePathPatterns("/api/docs/**")
             .excludePathPatterns("/docs/**")
             .excludePathPatterns("/api/kakao/oauth")
             .excludePathPatterns("/api/kakao/callback")

3e00b3b58beb686235d4df8148974debbb1e9dc9
RinSabbit <celena1004@naver.com>
2021-07-23 13:49:18 +0900

    refactor: test 메소드 분리

--- backend/goodday/src/test/java/seeuthere/goodday/member/controller/MemberAcceptanceTest.java
+++ backend/goodday/src/test/java/seeuthere/goodday/member/controller/MemberAcceptanceTest.java
@@ -5,17 +5,17 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import seeuthere.goodday.AcceptanceTest;
+import seeuthere.goodday.auth.dto.ProfileDto;
 import seeuthere.goodday.auth.infrastructure.JwtTokenProvider;
 import seeuthere.goodday.member.domain.Member;
 import seeuthere.goodday.member.dto.MemberRequest;
@@ -30,6 +30,18 @@ class MemberAcceptanceTest extends AcceptanceTest {
     @Autowired
     MemberService memberService;
 
+    @DisplayName("멤버 정보 가져오기")
+    @Test
+    void memberInfo() {
+        String path = "/api/members";
+        String token = jwtTokenProvider.createToken("1234");
+
+        ProfileDto response = getResponse("member/info", path, token)
+            .as(ProfileDto.class);
+
+        assertThat(response.getNickname()).isEqualTo("와이비");
+    }
+
     @DisplayName("멤버 수정 테스트")
     @Test
     public void memberUpdate() {
@@ -40,30 +52,7 @@ class MemberAcceptanceTest extends AcceptanceTest {
         String token = jwtTokenProvider.createToken("1234");
 
         // when
-        RestAssured.given(this.spec)
-            .filter(
-                document("member",
-                    preprocessRequest(prettyPrint()),
-                    preprocessResponse(prettyPrint()),
-                    requestFields(
-                        subsectionWithPath("name").description("사용자 닉네임"),
-                        subsectionWithPath("profileImage").description("사용자 프로필 이미지"),
-                        subsectionWithPath("memberId").description("사용자 고유 아이디")
-                    ),
-                    responseFields(
-                        subsectionWithPath("id").description("사용자 아이디"),
-                        subsectionWithPath("name").description("사용자 닉네임"),
-                        subsectionWithPath("profileImage").description("사용자 프로필 이미지"),
-                        subsectionWithPath("memberId").description("사용자 고유 아이디")
-                    )
-                )
-            )
-            .body(memberRequest)
-            .header("Authorization", "Bearer " + token)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-                .put(path)
-            .then().assertThat().statusCode(is(200));
+        getPutResponse("/member/update", path, memberRequest, token);
 
         //then
         Member member = memberService.find("1234");
@@ -71,4 +60,36 @@ class MemberAcceptanceTest extends AcceptanceTest {
         assertThat(member.getProfileImage()).isEqualTo("changedImage");
         assertThat(member.getMemberId()).isEqualTo("123454");
     }
+
+    private ExtractableResponse<Response> getResponse(String identifier, String path,
+        String token) {
+        return RestAssured.given(this.spec)
+            .filter(
+                document(identifier,
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint())
+                )
+            )
+            .header("Authorization", "Bearer " + token)
+            .when().get(path)
+            .then().statusCode(is(200))
+            .extract();
+    }
+
+    private void getPutResponse(String identifier, String path, MemberRequest memberRequest, String token) {
+        RestAssured.given(this.spec)
+            .filter(
+                document(identifier,
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint())
+                )
+            )
+            .body(memberRequest)
+            .header("Authorization", "Bearer " + token)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .when().put(path)
+            .then().assertThat().statusCode(is(200));
+    }
+
+
 }

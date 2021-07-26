a9d91ce16f860c920753961b34c8a5b2a7458d94
0imbean0 <tjsqls2003@gmail.com>
2021-07-26 22:14:41 +0900

    feat: private page 접근 제한 기능 추가
    Co-authored-by: 365kim <365kim@users.noreply.github.com>

--- frontend/src/contexts/UserContext.js
+++ frontend/src/contexts/UserContext.js
@@ -1,9 +1,10 @@
 import React, { useState, createContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useQuery } from 'react-query';
 
 import { httpRequest, storage } from '../utils';
-import { API_URL, STORAGE_KEY } from '../constants';
-import { useQuery } from 'react-query';
+import { API_URL, STORAGE_KEY, ROUTE, STATUS, PUBLIC_PATHS } from '../constants';
 
 const INITIAL_TOKEN = storage.local.get(STORAGE_KEY.TOKEN);
 
@@ -19,7 +20,7 @@ const fetchUserInfo = async ({ queryKey }) => {
   const response = await httpRequest.get(API_URL.TOKEN_VALIDATION, { accessToken });
 
   if (response.status === 401) {
-    return 'INVALID_TOKEN_ERROR';
+    throw new Error(STATUS.INVALID_TOKEN_ERROR);
   }
   return await response.json();
 };
@@ -27,6 +28,8 @@ const fetchUserInfo = async ({ queryKey }) => {
 export const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
+  const history = useHistory();
+  const { pathname } = useLocation();
   const [user, setUser] = useState(INITIAL_STATE);
   const { id, nickname, profileImage, token } = user;
   const isLogin = !!token;
@@ -41,31 +44,53 @@ export const UserContextProvider = ({ children }) => {
     setUser(INITIAL_STATE);
   };
 
+  const forceLogout = () => {
+    storage.local.remove(STORAGE_KEY.TOKEN);
+    setUser(INITIAL_STATE);
+    history.push(ROUTE.LOGIN.PATH);
+  };
+
   const { data: userInfo, error } = useQuery(['토큰유효성검사', INITIAL_TOKEN], fetchUserInfo, {
     enabled: !!INITIAL_TOKEN,
     staleTime: Infinity,
   });
 
   useEffect(() => {
+    if (PUBLIC_PATHS.map((v) => v.PATH).includes(pathname)) {
+      return;
+    }
+    forceLogout();
+  }, []);
+
+  useEffect(() => {
     if (!userInfo) {
       return;
     }
-    if (userInfo === 'INVALID_TOKEN_ERROR') {
-      logout();
-    }
     setUser({ ...userInfo, token: INITIAL_TOKEN });
   }, [userInfo]);
+
+  useEffect(() => {
+    if (!error) {
+      return;
+    }
+    if (error.message === STATUS.INVALID_TOKEN_ERROR) {
+      forceLogout();
+    }
+  }, [error]);
 
   return (
     <UserContext.Provider
       value={{
         user,
         setUser,
+
         id,
         nickname,
         profileImage,
         token,
+
         isLogin,
+
         login,
         logout,
       }}

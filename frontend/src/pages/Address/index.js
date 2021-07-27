50b6b06514191303db3353f8bc69746c299fb5b0
0imbean0 <tjsqls2003@gmail.com>
2021-07-27 16:35:58 +0900

    chore: AddressPage 미완성 기능 주석처리
    Co-authored-by: 365kim <365kim@users.noreply.github.com>

--- frontend/src/pages/Address/index.js
+++ frontend/src/pages/Address/index.js
@@ -14,11 +14,11 @@ export const AddressPage = () => {
   return (
     <main>
       <ContentArea>
-        <h2>내 주소를 등록해서 간단하게 추가해보아요.</h2>
+        <h2>내 주소를 간단하게 등록해보아요.</h2>
         <AddFormContextProvider formId={formId}>
           <UserAddressAddForm />
         </AddFormContextProvider>
-        <ul>
+        {/* <ul>
           {userAddressList?.map((address) => (
             <li key={address.id}>
               <button onClick={() => deleteAddress(address.id)}>
@@ -27,9 +27,7 @@ export const AddressPage = () => {
               </button>
             </li>
           ))}
-        </ul>
-
-        {'사용자 주소 리스트 불러와서 표시 + 수정 기능 / 삭제 기능'}
+        </ul> */}
       </ContentArea>
     </main>
   );

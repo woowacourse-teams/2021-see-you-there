68be1e03e84f0397e49f32dcaa5c21610bb6006c
Junyoung Choi <cjy1705@naver.com>
2021-08-05 18:53:54 +0900

    feat: spring actuator metric 설정 (#150)

--- backend/goodday/src/main/java/seeuthere/goodday/GooddayApplication.java
+++ backend/goodday/src/main/java/seeuthere/goodday/GooddayApplication.java
@@ -1,8 +1,10 @@
 package seeuthere.goodday;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class GooddayApplication {
 

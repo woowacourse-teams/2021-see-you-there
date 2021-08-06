c6c4cd72938ed9c434d85ad4c7e135b6e394df72
멍토 <daum7766@naver.com>
2021-08-06 11:11:51 +0900

    refactor: secret 정보를 yml로 이동

---
+++ backend/goodday/src/main/java/seeuthere/goodday/secret/SecretKey.java
@@ -0,0 +1,47 @@
+package seeuthere.goodday.secret;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecretKey {
+
+    public static String KAKAO_API_KEY;
+    public static String NAVER_API_KEY;
+    public static String NAVER_CLIENT_SECRET;
+    public static String JWT_SECRET_KEY;
+    public static Integer JWT_VALIDITY_TIME;
+    public static String TRANSPORT_API_KEY;
+
+    private SecretKey () {}
+
+    @Value("${security.kakao.apikey}")
+    public void setKakaoApiKey(String kakaoApiKey) {
+        KAKAO_API_KEY = kakaoApiKey;
+    }
+
+    @Value("${security.naver.apikey}")
+    public void setNaverApiKey(String naverApiKey) {
+        NAVER_API_KEY = naverApiKey;
+    }
+
+    @Value("${security.naver.client}")
+    public void setNaverClientSecret(String naverClientSecret) {
+        NAVER_CLIENT_SECRET = naverClientSecret;
+    }
+
+    @Value("${security.jwt.token.secret-key}")
+    public void setJwtSecretKey(String jwtSecretKey) {
+        JWT_SECRET_KEY = jwtSecretKey;
+    }
+
+    @Value("${security.jwt.token.expire-length}")
+    public void setJwtValidityTime(Integer jwtValidityTime) {
+        JWT_VALIDITY_TIME = jwtValidityTime;
+    }
+
+    @Value("${security.transport.apikey}")
+    public void setTransportApiKey(String transportApiKey) {
+        TRANSPORT_API_KEY = transportApiKey;
+    }
+}

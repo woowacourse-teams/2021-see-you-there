c5913ceb3d22f08ff57ac5ce2ce289c2a1450b0a
cjy17 <cjy1705@naver.com>
2021-08-09 21:32:32 +0900

    fix: Jenkinsfile 수정

--- backend/goodday/Jenkinsfile.groovy
+++ backend/goodday/Jenkinsfile.groovy
@@ -1,15 +1,10 @@
 node {
-    stage ('clone') {
+    stage('SCM') {
         checkout scm
     }
-    stage('build') {
-        sh 'cd back && ./gradlew api:clean api:build && ./gradlew chat:clean chat:build'
-    }
-    stage('SonarQube analysis') {
-        withSonarQubeEnv('SonarQube') {
-            sh 'cd back && ./gradlew --info sonarqube' +
-                    ' -Dsonar.projectKey=Jikgorae-' + env.BRANCH_NAME +
-                    ' -Dsonar.projectName=Jikgorae-' + env.BRANCH_NAME
+    stage('SonarQube Analysis') {
+        withSonarQubeEnv() {
+            sh "./gradlew sonarqube"
         }
     }
 }

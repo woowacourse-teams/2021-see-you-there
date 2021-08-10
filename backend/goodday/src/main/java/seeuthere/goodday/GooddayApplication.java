4e1c2d9dda9df30a69b96e8d97decbc67894072d
Junyoung Choi <cjy1705@naver.com>
2021-08-10 15:47:24 +0900

    refactor: aws error 안보이게 리팩토링 (#183)

--- backend/goodday/src/main/java/seeuthere/goodday/GooddayApplication.java
+++ backend/goodday/src/main/java/seeuthere/goodday/GooddayApplication.java
@@ -1,10 +1,8 @@
 package seeuthere.goodday;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAdminServer
 @SpringBootApplication
 public class GooddayApplication {
 

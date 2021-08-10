9c8c9a7d2c3e07f7bb8b90fdaa4c11b4a1c9422c
Junyoung Choi <cjy1705@naver.com>
2021-08-10 17:06:37 +0900

    [1] 로그 전략 수정 (#185)
    * refactor: aws error 안보이게 리팩토링
    
    * feat: 로그 전략 수정

--- backend/goodday/src/main/java/seeuthere/goodday/ControllerAdvice.java
+++ backend/goodday/src/main/java/seeuthere/goodday/ControllerAdvice.java
@@ -1,5 +1,6 @@
 package seeuthere.goodday;
 
+import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,8 @@ import seeuthere.goodday.exception.GoodDayException;
 @RestControllerAdvice
 public class ControllerAdvice {
 
+    private static final String MESSAGE = "예상치 못한 에러가 발생했습니다.";
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(GoodDayException.class)
@@ -23,8 +26,9 @@ public class ControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRunTimeException(RuntimeException e) {
-        logger.error(e.getMessage());
+        logger.error(e.toString());
+
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(new ErrorResponse(e.getMessage()));
+            .body(new ErrorResponse(MESSAGE));
     }
 }

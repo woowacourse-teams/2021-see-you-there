c56b86911b7697eeddc56d6d7f727833fd6dd361
0imbean0 <tjsqls2003@gmail.com>
2021-08-10 14:43:03 +0900

    chore: 카카오 애플리케이션 등록
    Co-authored-by: 365kim <365kim@users.noreply.github.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
+/* eslint no-undef: "off" */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './App';
 import GlobalStyle from './globalStyle';
 
-/* eslint-disable-next-line no-undef */
+Kakao.init(KAKAO);
 document.querySelector('#version').innerText = `version: ${VERSION}`;
 
 ReactDOM.render(

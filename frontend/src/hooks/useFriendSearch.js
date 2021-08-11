6c6a5ca06576dc7f6a75f692d1bae4d23537740c
365kim <365listener@gmail.com>
2021-08-11 23:18:13 +0900

    fix: 일치하는 ID 검색결과가 없을 때 UI 깨짐현상 해결
    Co-authored-by: 0imbean0 <0imbean0@users.noreply.github.com>

--- frontend/src/hooks/useFriendSearch.js
+++ frontend/src/hooks/useFriendSearch.js
@@ -6,22 +6,46 @@ import { httpRequest } from '../utils';
 import { API_URL, QUERY_KEY } from '../constants';
 
 export const useFriendSearch = () => {
-  const { token } = useContext(UserContext);
+  const { token, forceLogout } = useContext(UserContext);
   const [memberIdKeyword, setMemberIdKeyword] = useState('');
 
   const fetchFriendSearch = async ({ queryKey }) => {
     const [_, keyword] = queryKey;
     const response = await httpRequest.get(API_URL.FRIEND_SEARCH(keyword), { token });
 
+    if (response.status === 401) {
+      forceLogout();
+      return;
+    }
+    if (!response.ok) {
+      if (typeof response.body === 'object') {
+        const error = await response.json();
+        throw new Error(error.message);
+      }
+      const errorMessage = await response.text();
+      throw new Error(errorMessage);
+    }
+
     return await response.json();
   };
 
-  const { data: searchResult } = useQuery([QUERY_KEY.FRIEND_SEARCH, memberIdKeyword], fetchFriendSearch, {
+  const {
+    data: searchResult,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useQuery([QUERY_KEY.FRIEND_SEARCH, memberIdKeyword], fetchFriendSearch, {
     enabled: !!memberIdKeyword,
   });
 
   return {
     searchResult,
+    isLoading,
+    isSuccess,
+    isError,
+    errorMessage: error?.message,
+
     memberIdKeyword,
     setMemberIdKeyword,
   };

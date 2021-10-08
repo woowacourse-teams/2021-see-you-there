29a39f50a759280f27a67ecca0e9237d913d31cf
kim yu ki(Mung to) <45873044+daum7766@users.noreply.github.com>
2021-10-08 12:21:42 +0900

    [1] flyway 테이블 조건수정 (#266)
    * fix: flyway 문제 해결
    
    * refactor: type not null 조건 추가

--- backend/goodday/src/main/resources/db/migration/V7__alter-table-board-notnull.sql
+++
@@ -1,5 +0,0 @@
-﻿UPDATE board SET content = 'null' WHERE content IS NULL;
-UPDATE board SET title = 'null' WHERE title IS NULL;
-
-ALTER TABLE board MODIFY content text NOT NULL;
-ALTER TABLE board MODIFY title varchar(255) NOT NULL;

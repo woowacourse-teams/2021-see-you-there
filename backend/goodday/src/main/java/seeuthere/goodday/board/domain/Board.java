61c13a1b34727d60a17f38ea8799ec3d04837f94
mungto <daum7766@naver.com>
2021-10-08 11:09:50 +0900

    refactor: content 타입을 text로 변경

--- backend/goodday/src/main/java/seeuthere/goodday/board/domain/Board.java
+++ backend/goodday/src/main/java/seeuthere/goodday/board/domain/Board.java
@@ -11,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import org.hibernate.annotations.CreationTimestamp;
@@ -34,6 +35,7 @@ public class Board {
 
     private String title;
 
+    @Lob
     private String content;
 
     @Enumerated(value = EnumType.STRING)
---
+++ backend/goodday/src/main/resources/db/migration/V6__alter-table-board.sql
@@ -0,0 +1,1 @@
+﻿ALTER TABLE board MODIFY content text;

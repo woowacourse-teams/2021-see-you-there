77a949637dd0f5d11379c7eeaafa1c9f84b4b101
0imbean0 <tjsqls2003@gmail.com>
2021-10-22 14:10:25 +0900

    chore: 구글애널리틱스 ID 변경

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -57,7 +57,7 @@ const getConfig = ({ isDev, isAnalyzeMode }) => ({
     new webpack.DefinePlugin({
       VERSION: JSON.stringify(package.version),
       KAKAO: JSON.stringify('1b0ee776c585e8fb3a1ab8da4a771a75'),
-      GTAG: JSON.stringify('G-F47EFX7Z7B'),
+      GTAG: JSON.stringify('G-BPV8LW9CRN'),
     }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
